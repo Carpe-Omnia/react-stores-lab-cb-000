3118c2673a9a8e483ef23cd9150c067ce839505f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-12 19:30:16 +0000

    Automatically backed up by Learn

--- stores/Store.js
+++ stores/Store.js
@@ -1,5 +1,13 @@
 class Store {
-
+  constructor(initialState) {
+    this.state = initialState ;
+  }
+  setState (state) {
+    this.state = state ;
+  }
+  getState () {
+    return this.state ;
+  }
 }
 
 export default Store;
