285a97f6a93dfb249b4a3d7082ba75e50e1936cc
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-12 19:48:05 +0000

    Automatically backed up by Learn

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,9 @@
 import counterStore from '../stores/counterStore.js'
 
 function increment() {
-
+  counterStore.increment() ;
 }
 function decrement() {
-
+  counterStore.decrement() ;
 }
 export default {increment, decrement}
